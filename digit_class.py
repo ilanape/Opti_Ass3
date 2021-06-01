5759ed6df0fb6284fa97d9deb0130bf50c366304
ilanape <ilanape@post.bgu.ac.il>
2021-06-01 10:15:47 +0300

    4b

--- digit_class.py
+++ digit_class.py
@@ -8,14 +8,13 @@ def sigmoid(x):
 
 def func(X, w, c1, c2):
     # Linear Regression Objective
-    n = np.shape(X)[0]
     m = np.shape(X)[1]
-    sigm_XTw = [sigmoid(np.dot(X[:, i], w)) for i in range(m)]
-    Fw = (-1 / m) * (np.dot(c1, sigm_XTw)
+    sigm_XTw = np.array([sigmoid(np.dot(X[:, i], w)) for i in range(m)])
+    Fw = (-1 / m) * (np.dot(c1, np.log(sigm_XTw))
                      + np.dot(c2, np.log(np.ones(m) - sigm_XTw)))
 
     # Gradient
-    Grad = (1 / m) * X @ np.subtract(sigm_XTw, c1)
+    Grad = (1 / m) * X @ (sigm_XTw - c1)
 
     # Hessian
     D_diag = np.multiply(sigm_XTw, np.ones(m) - sigm_XTw)
@@ -27,12 +26,13 @@ def func(X, w, c1, c2):
 
 # Gradient test
 n = 20
+m = 25
 x = np.random.rand(n)
 d = np.random.rand(n)
 eps = 0.1
-X = np.random.rand(n, n)
-c1 = np.random.randint(2, size=n)
-c2 = np.ones(n) - c1
+X = np.random.rand(n, m)
+c1 = np.random.randint(2, size=m)
+c2 = np.ones(m) - c1
 
 x_axis = []
 y_axis = []
@@ -40,18 +40,21 @@ y1_axis = []
 y2_axis = []
 y3_axis = []
 
-for k in range(8):
+for k in range(1, 9):
     eps = eps * (0.5 ** k)
     x_axis.append(k)
     Fx, gradx, hessx = func(X, x, c1, c2)
     Fx_ed, gradx_ed, hessx_ed = func(X, x + eps * d, c1, c2)
 
     y_axis.append(np.abs(Fx_ed - Fx))
-    y1_axis.append(np.abs(Fx_ed - Fx - eps * np.dot(d, gradx)))
+    y1_axis.append(np.abs(Fx_ed - Fx - eps * np.dot(gradx, d)))
     y2_axis.append(np.linalg.norm(gradx_ed - gradx))
     y3_axis.append(np.linalg.norm(gradx_ed - gradx - eps * hessx @ d))
 
 plt.figure()
+plt.title('Gradient and Jacobian verification tests')
+plt.xlabel("k iteration")
+plt.ylabel("error")
 plt.semilogy(x_axis, y_axis, 'r', label="|f(x+ed)-f(x)|")
 plt.semilogy(x_axis, y1_axis, 'g', label="|f(x+ed)-f(x) - e*d*grad(x)|")
 plt.semilogy(x_axis, y2_axis, 'b', label="||grad(x+ed)-grad(x)||")
